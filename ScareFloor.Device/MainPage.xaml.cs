4c2303b951a24ec24205e0435ba1237339096cd5
Jared Porcenaluk <jared@NebbiaTech.com>
2017-03-07 05:06:20 -0500

    Moved project to more appropriately named project.

---
+++ ScareFloor.Device/MainPage.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Audio;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Text;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ScareFloor.Device
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private const int LED_PIN = 5;
+        private GpioPin pin;
+        private GpioPinValue pinValue;
+        private DeviceClient deviceClient;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            InitGpio();
+            InitDeviceClient();
+            ReceiveMessage();
+        }
+
+
+        private void InitGpio()
+        {
+            var gpio = GpioController.GetDefault();
+
+            // Show an error if there is no GPIO controller
+            if (gpio == null)
+            {
+                pin = null;
+                GpioStatus.Text = "There is no GPIO controller on this device.";
+                return;
+            }
+
+            pin = gpio.OpenPin(LED_PIN);
+            pinValue = GpioPinValue.High;
+            pin.Write(pinValue);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+
+            GpioStatus.Text = "GPIO pin initialized correctly.";
+        }
+
+        private void InitDeviceClient()
+        {
+            string iotHubUri = "ScareFloor.azure-devices.net"; // ! put in value !
+            string deviceId = "scarefloor"; // ! put in value !
+            string deviceKey = "nJQ8o1NRGh6N39OD6dwpICXDi/n+KMZ0dXQpfPB0VqA="; // ! put in value !
+
+            deviceClient = DeviceClient.Create(iotHubUri,
+                    AuthenticationMethodFactory.
+                        CreateAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey),
+                    TransportType.Http1);
+        }
+
+        private void LightToggle(object sender, RoutedEventArgs e)
+        {
+            if (pinValue == GpioPinValue.High)
+            {
+                pinValue = GpioPinValue.Low;
+                pin.Write(pinValue);
+                LightStatus.Text = "LOW";
+            }
+            else
+            {
+                pinValue = GpioPinValue.High;
+                pin.Write(pinValue);
+                LightStatus.Text = "HIGH";
+            }
+        }
+
+        private async void SendMessage(object sender, RoutedEventArgs e)
+        {
+            var str = "Hello, Cloud!";
+            var message = new Message(Encoding.ASCII.GetBytes(str));
+
+            await deviceClient.SendEventAsync(message);
+            MessageStatus.Text = "Sent";
+        }
+
+        private async void ReceiveMessage()
+        {
+            Console.WriteLine("\nReceiving cloud to device messages from service");
+            while (true)
+            {
+
+                Message receivedMessage = await deviceClient.ReceiveAsync();
+                if (receivedMessage == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received message: {0}", Encoding.ASCII.GetString(receivedMessage.GetBytes()));
+                Console.ResetColor();
+
+                MessageReceived.Text = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+
+                await deviceClient.CompleteAsync(receivedMessage);
+            }
+        }
+    }
+}
