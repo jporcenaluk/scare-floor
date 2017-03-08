2418da59caef805f3c541b62d54e5952cf64e7f8
Jared Porcenaluk <jared@NebbiaTech.com>
2017-03-08 04:15:37 -0500

    Added WPF application with speech to text abilities.

---
+++ ScareFloor.WPFVoice/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ScareFloor.WPFVoice
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
